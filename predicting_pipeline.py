3a6d98471473cdda25adcf64525aa4a63223cd74
Gianluca Rossi <grossi@quantcast.com>
2016-01-18 11:50:25 -0500

    unify predicting functions for all models into one single function

--- predicting_pipeline.py
+++ predicting_pipeline.py
@@ -13,7 +13,7 @@ def format_output_namefile(fname, fmt='%Y%m%d-%H%M%S_{fname}'):
     return datetime.datetime.now().strftime(fmt).format(fname=fname)
 
 
-def predict_decision_tree(X_test, ids, model):
+def make_predictions(X_test, ids, model, filename="results.csv"):
     # predict most likely destination for each user, using decision tree classifier
     predictions = model.predict_proba(X_test)
     probs = pd.DataFrame(predictions, columns=model.classes_, index=ids)
@@ -24,7 +24,7 @@ def predict_decision_tree(X_test, ids, model):
     n = 1
     tot = len(probs.index) + 1
     for id in probs.index:
-        df = probs[probs.index==id]
+        df = probs[probs.index == id]
         destinations = []
         count = 0
         while count < 5:
@@ -41,44 +41,7 @@ def predict_decision_tree(X_test, ids, model):
         submission = pd.concat([submission, pd.DataFrame({"aid": key, "country": value})])
 
     result_path = "./predictions/"
-    result_file_name_end = "results_decision_trees.csv"
-    result_file_name = format_output_namefile(result_file_name_end)
-    result_file_path = "".join((result_path, result_file_name))
-
-    submission.to_csv(result_file_path, index=False)
-    print("Output saved in", result_file_name)
-
-
-def predict_logistic_regression(X_test, ids, model):
-    # return likelihood on a probability scale for each destination
-    predictions = model.predict_proba(X_test)
-    probs = pd.DataFrame(predictions, columns=model.classes_, index=ids)
-
-    # select top 5 destinations for each user
-    results = {}
-    n = 1
-    tot = len(probs.index) + 1
-    for id in probs.index:
-        df = probs[probs.index==id]
-        destinations = []
-        count = 0
-        while count < 5:
-            # TODO: write results to file within the loop
-            destination = df.idxmax(axis=1)
-            destinations.append(destination[0])
-            df.drop(destination, inplace=True, axis=1)
-            count += 1
-        results[id] = destinations
-        n += 1
-        print("Completed:", n, "/", tot)
-
-    submission = pd.DataFrame(columns=["aid", "country"])
-    for key, value in results.items():
-        submission = pd.concat([submission, pd.DataFrame({"aid": key, "country": value})])
-
-    result_path = "./predictions/"
-    result_file_name_end = "results_logistic_regression.csv"
-    result_file_name = format_output_namefile(result_file_name_end)
+    result_file_name = format_output_namefile(filename)
     result_file_path = "".join((result_path, result_file_name))
 
     submission.to_csv(result_file_path, index=False)
