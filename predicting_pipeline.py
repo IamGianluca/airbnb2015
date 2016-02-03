9fcf894e7161f31e44174a0715b07c9096dd16f8
Gianluca Rossi <grossi@quantcast.com>
2016-02-03 22:28:27 +0000

    BUG: fix indexing which was preventing to write the results into a .csv file; also rename a few variables

--- predicting_pipeline.py
+++ predicting_pipeline.py
@@ -6,40 +6,40 @@
 
 import pandas as pd
 import datetime
+import pickle
 
 
-def format_output_namefile(fname, fmt='%Y%m%d-%H%M%S_{fname}'):
-    # add current datetime to the result file name; useful to keep track of multiple solutions
-    return datetime.datetime.now().strftime(fmt).format(fname=fname)
+def format_output_filename(filename, fmt='%Y%m%d-%H%M%S_{fname}'):
+    # add current datetime to the result file name
+    return datetime.datetime.now().strftime(fmt).format(fname=filename)
 
 
-def make_predictions(X_test, ids, model, centre=False, filename="results.csv"):
+def make_predictions(test_set, model, centre=False, filename="results.csv"):
+    with open('./data/test_ids.pickle', 'rb') as f:
+        ids = pickle.load(f)
+
     # if the model is not scale independent, scale the test dataset
     if centre is True:
         from sklearn import preprocessing
-        X_test = preprocessing.scale(X_test)
+        test_set = preprocessing.scale(test_set)
 
     # predict most likely destination for each user, using decision tree classifier
-    predictions = model.predict_proba(X_test)
+    predictions = model.predict_proba(test_set)
     probs = pd.DataFrame(predictions, columns=model.classes_, index=ids)
 
     # TODO: this should be a function, need refactoring
     # select top 5 destinations for each user
     results = {}
-    n = 1
-    tot = len(probs.index) + 1
-    for id in probs.index:
-        df = probs[probs.index == id]
+    for idx in probs.index:
+        row = probs.iloc[idx]
         destinations = []
         count = 0
         while count < 5:
-            destination = df.idxmax(axis=1)
-            destinations.append(destination[0])
-            df.drop(destination, inplace=True, axis=1)
+            destination = row.idxmax(axis=1)
+            destinations.append(destination)
+            row.drop(destination, inplace=True, axis=0)
             count += 1
-        results[id] = destinations
-        n += 1
-        # print("Completed:", n, "/", tot)
+        results[idx] = destinations
 
     # TODO: there is no need to create a data frame for this. Should output to text file directly from the for loop
     submission = pd.DataFrame(columns=["aid", "country"])
@@ -47,7 +47,7 @@ def make_predictions(X_test, ids, model, centre=False, filename="results.csv"):
         submission = pd.concat([submission, pd.DataFrame({"aid": key, "country": value})])
 
     result_path = "./predictions/"
-    result_file_name = format_output_namefile(filename)
+    result_file_name = format_output_filename(filename)
     result_file_path = "".join((result_path, result_file_name))
 
     submission.to_csv(result_file_path, index=False)
