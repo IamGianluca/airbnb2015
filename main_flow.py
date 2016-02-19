5f719793cb05c68a13c5fe1e4e0965d4a92cc3fb
Gianluca Rossi <grossi@quantcast.com>
2016-02-19 10:27:44 +0000

    update main flow

--- main_flow.py
+++ main_flow.py
@@ -1,28 +1,38 @@
 """
 @author: Gianluca Rossi
 @project: Airbnb Recruiting Challenge (New User Bookings)
-@title: Control board to conveniently launch featurizer, training and prediction flows
+@title: Pipeline launcher
 """
+
 
 import featurizer as featurize
 import training_pipeline as train
 import predicting_pipeline as predict
 
 if __name__ == "__main__":
+    # pre-processing and feature extraction step
+    # featurize.create_features(True)
+    # featurize.create_features(False)
 
-    # featurizer flow
-    training_features = featurize.create_features(True)
-    featurize.create_features(False, training_features)
+    # split training and test sets
+    X_train, X_test, y_train = train.prepare_dataset()
 
-    # create training and test sets
-    X_train, X_test, y_train, ids_test = train.prepare_dataset()
+    # train logistic regressions
+    lr = train.train_logistic_regression(X_train, y_train)
+    predict.make_predictions(test_set=X_test, model=lr, centre=False, filename="results_lr.csv")
 
-    # train decision trees classifier and pull results
-    tree_model = train.train_decision_tree(X_train, y_train)
-    predict.predict_decision_tree(X_test, ids_test, tree_model)
+    # # train decision trees classifier
+    tree = train.train_decision_tree(X_train, y_train)
+    predict.make_predictions(test_set=X_test, model=tree, centre=False, filename="results_tree.csv")
+    #
+    # # train random forest classifier
+    rf = train.train_random_forest(X_train, y_train)
+    predict.make_predictions(test_set=X_test, model=rf, centre=False, filename="results_rf.csv")
 
-    # train random forest classifier and pull results
-    # rf_model = tp.train_random_forest(X_train, X_test, y_train, y_test)
-    # predict.predict_random_forest(rf_model, features)
+    # train knn; accuracy close to tree models but very slow to fit
+    # knn = train.train_svm(X_train, y_train)
+    # predict.make_predictions(X_test, ids_test, knn, centre=True, filename="results_knn.csv")
 
-    # train xgboost classifier and pull results
+    # train svm; accuracy close to tree models but very slow to fit
+    # svc = train.train_svm(X_train, y_train)
+    # predict.make_predictions(X_test, ids_test, svc, centre=True, filename="results_svm_rbf.csv")
